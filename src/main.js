4ac502bd810e267c696a7061fec6d13b4cd9229a
Moonhokyun <soulty321@gmail.com>
2025-05-28 00:10:56 +0900

    vuetify setting

--- src/main.js
+++ src/main.js
@@ -5,10 +5,15 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { createVuetify } from 'vuetify'
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css' // 아이콘 사용시 필요
 
+const vuetify = createVuetify()
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
