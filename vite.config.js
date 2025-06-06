bf9c737cda17a99ac52b09eccbd7838aea3ae8e1
Moonhokyun <soulty321@gmail.com>
2025-06-06 10:08:52 +0900

    link-seetup

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,14 @@ import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 import vuetify from "vite-plugin-vuetify";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
     vuetify({
       autoImport: true,
-      styles: {
-        configFile: "src/assets/scss/variables.scss", // 방금 만든 SCSS 파일 경로
-      },
+      styles: { configFile: "src/assets/scss/variables.scss" },
     }),
   ],
   resolve: {
